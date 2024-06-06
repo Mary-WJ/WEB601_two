bf128411798671a684bf32d0132cb89203c0ce3d
Mary-WJ <mary-par@live.nmit.ac.nz>
2024-06-07 10:37:52 +1200

    the database has been successfully set up

--- src/db/database.js
+++ src/db/database.js
@@ -1,2 +1,32 @@
-//connect to mangodb
-//create a schema for the sign up and login to handle authentication
+require('dotenv').config({ path: '../../.env' });   //connect to mangodb
+
+const mongoose = require('mongoose');
+const express = require('express');
+
+const cors = require('cors');
+const app = express();
+
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+
+//connection to db
+const PORT = process.env.PORT || 3000;
+const dbUser = process.env.DB_USERNAME;
+const dbPass = process.env.DB_PASSWORD;
+
+//here goes the schema
+
+
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@notedb.gckbyvw.mongodb.net/`)
+.then(() => {
+    console.log('connected to db');
+    app.listen(PORT, () => {
+        console.log(`listening on port ${PORT}`);
+    })
+})
+.catch((error) => {
+    console.log('could not connect to db', error);
+})
